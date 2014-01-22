18bb2909860c7554ec7be6407e34dd6215037521
iTitou <titouanchristophe@gmail.com>
2014-01-22 17:43:11 +0100

    [enh] Use context manager

--- notify_notes.py
+++ notify_notes.py
@@ -8,37 +8,37 @@ from send_mail import mail
 CACHEFILE = 'available_notes.json'
 
 class Cache:
-	def __init__(self, filename, initial={}):
-		self.filename = filename
-		self.initial = initial
+    def __init__(self, filename=CACHEFILE, initial={}):
+        self.filename = filename
+        self.initial = initial
 
-	def __enter__(self):
-		self.cache = self.initial
-		try:
-			self.cache = json.loads(open(self.filename).read())
-		except:
-			pass
-		return self.cache
+    def __enter__(self):
+        self.cache = self.initial
+        try:
+            self.cache = json.loads(open(self.filename).read())
+        except:
+            pass
+        return self.cache
 
-	def __exit__(self, exc_type, exc_value, backtrace):
-		if exc_type is None:
-			try:
-				open(self.filename, 'w').write(json.dumps(self.cache))
-			except:
-				return False
-			return True
+    def __exit__(self, exc_type, exc_value, backtrace):
+        if exc_type is None:
+            try:
+                open(self.filename, 'w').write(json.dumps(self.cache))
+            except:
+                return False
+            return True
 
+if __name__ == "__main__":
+    with Cache() as cache:
+        diff = {}
+        for mnemonic, name, ects, note in get_notes(ULB_USER, ULB_PASSWORD):
+            if note is not None and mnemonic not in cache:
+                diff[mnemonic] = name
 
-with Cache(CACHEFILE) as cache:
-	diff = {}
-	for mnemonic, name, ects, note in get_notes(ULB_USER, ULB_PASSWORD):
-		if note is not None and mnemonic not in cache:
-			diff[mnemonic] = name
+        if len(diff) > 0:
+            changed = [k+' - '+diff[k] for k in diff]
+            message = "Les notes des cours suivants sont sorties: \n" 
+            message += "\n".join(changed) + "\nhttps://mon-ulb.ulb.ac.be/"
+            mail(TO_ADDR, "", message)
 
-	if len(diff) > 0:
-		changed = [k+' - '+diff[k] for k in diff]
-		message = "Les notes des cours suivants sont sorties: \n" 
-		message += "\n".join(changed) + "\nhttps://mon-ulb.ulb.ac.be/"
-		mail(TO_ADDR, "", message)
-
-	cache.update(diff)
+        cache.update(diff)
