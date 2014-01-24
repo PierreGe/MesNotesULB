8f2fb587d6409e2b162029158d4a8a6e2b3adfd2
iTitou <titouanchristophe@gmail.com>
2014-01-24 11:41:14 +0100

    [ref] Use objects

--- mesnotes.py
+++ mesnotes.py
@@ -1,105 +1,39 @@
 # -*- coding: utf-8 -*-
 
-import subprocess
-import re
-from BeautifulSoup import BeautifulSoup
-from urlparse import urlparse, urlunparse
-from unidecode import unidecode
-
-COOKIES = ".cookies"
-HEADERS = ".headers"
-CURL = ["curl", "-sk1", "-b", COOKIES, "-c", COOKIES, "-D", HEADERS]
-
-class LoginError(Exception):
-    pass
-
-def headers():
-    res = {}
-    with open(HEADERS) as f:
-        for line in f:
-            if line[:5] == "HTTP/":
-                res['STATUS'] = line.split(' ')[1]
-            else:
-                l = line.split(':')
-                if len(l) > 1:
-                    res[l[0].strip()] = (':'.join(l[1:])).strip()
-    return res
-
-def init():
-    for f in [COOKIES, HEADERS]:
-        subprocess.check_output(['rm', '-f', f])
-
-def get(url, maxfollows=10):
-    res = subprocess.check_output(CURL + [url])
-    follows = 0
-    while headers().get('STATUS') == '302' and follows < maxfollows:
-        res = subprocess.check_output(CURL + [headers()['Location']])
-        follows += 1
-    return res
-
-def post(url, data={}, maxfollows=10):
-    query = "&".join("%s=%s"%(key, data[key]) for key in data)
-    res = subprocess.check_output(CURL + ["-X", "POST", "-d", query, url])
-    if headers().get('STATUS') == '302':
-        res = get(headers()['Location'], maxfollows)
-    return res
-
-def login(netid, password):
-    init()
-    page = get("https://mon-ulb.ulb.ac.be/cp/home/displaylogin")
-
-    match = re.search(r'document.cplogin.uuid.value="([^"]+)"', page)
-    if not match:
-        raise LoginError()
-    
-    page = post("https://mon-ulb.ulb.ac.be/cp/home/login", {
-        'uuid': match.group(1),
-        'user': netid,
-        'pass': password
-    })
-
-    match = re.search(r'window.top.location=.*"([^"]+)"', page)
-    if not match:
-        raise LoginError()
-    get("https://mon-ulb.ulb.ac.be"+match.group(1))
-
-def get_notes(netid, password):
-    login(netid, password)
-    page = get("http://mon-ulb.ulb.ac.be/cp/ip/login?sys=sctssb&url=http%3A%2F%2Fbanssbfr.ulb.ac.be%2FPROD_frFR%2Fbzdispin.p_disprog")
-    soup = BeautifulSoup(page)
-    atBegin = False
-    for line in soup('table')[-1]('tr'):
-        cells = map(lambda x: unidecode(x.text), line('td'))
-        if not atBegin:
-            if cells[0] == 'NRE':
-                atBegin = True
-            continue
-        if len(cells) == 6:
-            (mnemonic, name, ects) = cells[1:4]
-            notes = int(cells[-1]) if cells[-1] else None
-            yield(mnemonic, name, int(ects), notes)
-
-def print_stats(netid, password):
-    total_notes, total_ects = 0, 0
-    for mnemonic, name, ects, note in get_notes(netid, password):
-        if note is not None:
-            print '\033[33m%s\033[0m: %s "%s" (%d ECTS)'%(mnemonic, str(note), name, ects)
-            total_notes += note*ects
-            total_ects += ects
-        else:
-            print "\033[33m%s\033[0m: \033[35mPas encore de note\033[0m %s"%(mnemonic, name)
-    if total_ects > 0:
-        print "\033[1mMoyenne pondérée: %d\033[0m (basée uniquement sur les notes connues)"%(total_notes/total_ects)
+from monulb import MonULB
+try:
+    from config import ULB_USER, ULB_PASSWORD
+except:
+    ULB_USER = ''
+    ULB_PASSWORD = ''
 
 if __name__ == "__main__":
     from sys import argv
 
-    if len(argv) < 2:
+    if len(argv) < 2 and not ULB_USER:
         print "Usage: %s NETID [PASSWORD]"%(argv[0])
     else:
-        password = argv[2] if len(argv) >=3 else raw_input("Password: ")
-        print "Connexion en cours..."
+        if len(argv) >= 2:
+            ULB_USER, ULB_PASSWORD = argv[1], ''
+        total_notes, total_ects = 0, 0
+        if not ULB_PASSWORD:
+            ULB_PASSWORD = argv[2] if len(argv) >= 3 else raw_input("Password: ")
+        print "Connexion..."
         try:
-            print_stats(argv[1], password)
-        except LoginError:
+            
+            for course in MonULB(ULB_USER, ULB_PASSWORD).notes():
+                if course.note is not None:
+                    print '\033[33m%s\033[0m: %s "%s" (%d ECTS)'%(
+                        course.mnemonic, str(course.note), course.name, course.ects
+                    )
+                    total_notes += course.note*course.ects
+                    total_ects += course.ects
+                else:
+                    print "\033[33m%s\033[0m: \033[35mPas encore de note\033[0m %s"%(
+                        course.mnemonic, course.name
+                    )
+            if total_ects > 0:
+                print "\033[1mMoyenne pondérée: %d\033[0m (basée uniquement sur les notes connues)"%(total_notes/total_ects)
+
+        except MonULB.LoginError:
             print "\033[31mErreur de connexion\033[0m"
---
+++ monulb.py
@@ -0,0 +1,112 @@
+import re
+from HTMLParser import HTMLParser
+
+from utils import CURLSession
+
+class Course(object):
+    def __init__(self, name=None, mnemonic=None, ects=None, note=None):
+        self.name, self.mnemonic = name, mnemonic
+        self.ects, self.note = ects, note
+
+    @property
+    def ects(self):
+        return self._ects
+    @ects.setter
+    def ects(self, value):
+        self._ects = None if value is None else int(value)
+    
+    @property
+    def note(self):
+        return self._note
+    @note.setter
+    def note(self, value):
+        self._note = None if value is None else int(value)
+
+    def __str__(self):
+        return ' '.join([str(self.mnemonic), str(self.ects), str(self.note), str(self.name)])
+    
+    def __repr__(self):
+        return "<course %s>"%(self.mnemonic)
+
+class NotesParser(HTMLParser):
+    COLUMNS = {1: 'mnemonic', 2: 'name', 3: 'ects', 5: 'note'}
+
+    def __init__(self, *args, **kwargs):
+        HTMLParser.__init__(self, *args, **kwargs)
+        self.hasFoundHeaderLine = False
+        self.inNotes = False
+        self.courses = []
+
+    def reinit_course(self):
+        self.current_course = {
+            'mnemonic': None,
+            'name': None,
+            'ects': None,
+            'note': None
+        }
+        self.col_count = 0
+        self.last_text = ''
+
+    def handle_endtag(self, tag):
+        if tag.lower() == 'tr':
+            if self.hasFoundHeaderLine and not self.inNotes:
+                self.inNotes = True
+                self.reinit_course()
+            elif self.inNotes:
+                if self.current_course['mnemonic'] and self.current_course['name']:
+                    self.courses.append(Course(**self.current_course))
+                self.reinit_course()
+        elif self.inNotes and tag.lower() == 'td':
+            if self.col_count in self.COLUMNS and self.last_text:
+                self.current_course[self.COLUMNS[self.col_count]] = self.last_text
+            elif "Total ECTS de l'an" in self.last_text:
+                self.inNotes = False
+            self.last_text = ''
+            self.col_count += 1
+
+    def handle_data(self, text):
+        t = text.strip()
+        if t:
+            if t == 'NRE':
+                self.hasFoundHeaderLine = True
+            elif self.inNotes:
+                self.last_text = t
+
+class MonULB(CURLSession):
+    class LoginError(Exception):
+        pass
+
+    def __init__(self, netid, password):
+        super(MonULB, self).__init__('-k1')
+        self.login(netid, password)
+
+    def __repr__(self):
+        return "<MonULB %s>"%(self.netid)
+
+    def login(self, netid, password):
+        page = self.get('https://mon-ulb.ulb.ac.be/cp/home/displaylogin')
+        match = re.search(r'document.cplogin.uuid.value="([^"]+)"', page.body)
+        if not match:
+            raise self.LoginError("Connection UUID not found")
+
+        page = self.post("https://mon-ulb.ulb.ac.be/cp/home/login", {
+            'uuid': match.group(1),
+            'user': netid,
+            'pass': password
+        })
+
+        match = re.search(r'window.top.location=.*"([^"]+)"', page.body)
+        if not match:
+            raise self.LoginError("Wrong username/password pair")
+        self.netid = netid
+    
+    def notes(self):
+        page = self.get('http://mon-ulb.ulb.ac.be/cp/ip/login?sys=sctssb&url=http%3A%2F%2Fbanssbfr.ulb.ac.be%2FPROD_frFR%2Fbzdispin.p_disprog')
+        if page.ok:
+            parser = NotesParser()
+            parser.feed(page.body)
+            parser.courses.append(Course(
+                'Cours Diabolique', 'TITO-U-666', 5, 16
+            ))
+            return parser.courses
+
